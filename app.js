3006dc2cc091bdcc7a2f5c8c6120f2fc2ae03599
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2022-05-30 14:45:26 +0200

    add feature: search the api on input change

--- app.js
+++ app.js
@@ -20,5 +20,28 @@ const fetchFilms = async () => {
   console.log(response.data);
 };
 
-fetchFilm();
-fetchFilms();
+// fetchFilm();
+// fetchFilms();
+
+const searchedData = async (searchTerm) => {
+  const response = await axios.get('http://omdbapi.com/', {
+    params: {
+      apikey: '891d1ad5',
+      s: searchTerm,
+    },
+  });
+
+  console.log(response.data);
+};
+
+const input = document.querySelector('input');
+let timeoutId;
+const onInput = (event) => {
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(() => {
+    searchedData(event.target.value);
+  }, 400);
+};
+input.addEventListener('input', onInput);
