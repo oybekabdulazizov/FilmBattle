d9011c67358089c3fa7dd468ed32d42e882ea119
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2022-05-31 12:38:52 +0200

    fix the searched film menu and get it fully working

--- app.js
+++ app.js
@@ -12,18 +12,55 @@ const searchData = async (searchTerm) => {
 
   return response.data.Search;
 };
-
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+    <label><b>Search For A Film</b></label>
+    <input class='input'/>
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results">
+            </div>
+        </div>
+    </div>
+`;
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
 const onInput = async (event) => {
   const films = await searchData(event.target.value);
+
+  if (!films.length) {
+    dropdown.classList.remove('is-active');
+    return;
+  }
+
+  resultsWrapper.innerHTML = ``;
+
+  dropdown.classList.add('is-active');
   for (let film of films) {
-    const div = document.createElement('div');
-    div.innerHTML = `
-        <img src="${film.Poster}}" />
-        <h1>${film.Title}</h1>
+    const option = document.createElement('a');
+    const imgSrc =
+      film.Poster === 'N/A' ? 'assets/film-placeholder.jpg' : film.Poster;
+
+    option.classList.add('dropdown-item');
+    option.innerHTML = `
+        <img src="${imgSrc}" /> 
+        ${film.Title}
     `;
 
-    document.querySelector('#target').appendChild(div);
+    option.addEventListener('click', () => {
+      dropdown.classList.remove('is-active');
+      input.value = `${film.Title}`;
+    });
+
+    resultsWrapper.appendChild(option);
   }
 };
 input.addEventListener('input', debounce(onInput, 400));
+
+document.addEventListener('click', (event) => {
+  if (!root.contains(event.targe)) {
+    dropdown.classList.remove('is-active');
+  }
+});
