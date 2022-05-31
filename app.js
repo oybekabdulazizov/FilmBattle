185360f4751bed921fe3ec21f8684a1f5445b961
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2022-05-31 10:40:51 +0200

    implement debounce and move it to utilities.js file

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-const fetchFilm = async () => {
-  const response = await axios.get('http://www.omdbapi.com/', {
-    params: {
-      apikey: '891d1ad5',
-      i: 'tt0848228',
-    },
-  });
-
-  console.log(response.data);
-};
-
-const fetchFilms = async () => {
-  const response = await axios.get('http://www.omdbapi.com/', {
-    params: {
-      apikey: '891d1ad5',
-      s: 'batman',
-    },
-  });
-
-  console.log(response.data);
-};
-
-// fetchFilm();
-// fetchFilms();
-
 const searchedData = async (searchTerm) => {
   const response = await axios.get('http://omdbapi.com/', {
     params: {
@@ -35,13 +10,7 @@ const searchedData = async (searchTerm) => {
 };
 
 const input = document.querySelector('input');
-let timeoutId;
-const onInput = (event) => {
-  if (timeoutId) {
-    clearTimeout(timeoutId);
-  }
-  timeoutId = setTimeout(() => {
-    searchedData(event.target.value);
-  }, 400);
-};
+const onInput = debounce((event) => {
+  searchedData(event.target.value);
+});
 input.addEventListener('input', onInput);
